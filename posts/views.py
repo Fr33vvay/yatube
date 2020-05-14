684543e646f68ab5b06eddf9031a7f9fff6a0613
Илья <7634216@gmail.com>
2020-05-14 19:52:57 +0300

    profiles

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm
-from .models import Group, Post
+from .models import Group, Post, User
 
 
 def index(request):
@@ -38,3 +38,31 @@ def new_post(request):
             return redirect('index')
     form = PostForm()
     return render(request, 'new_post.html', {'form': form})
+
+
+def profile(request, username):
+    member = User.objects.get(username=username)
+    post_list = Post.objects.filter(author=member).order_by('-pub_date')
+    post_count = len(post_list)
+    paginator = Paginator(post_list, 10)
+
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, "profile.html", {'member': member,
+                                            'post_count': post_count,
+                                            'page': page,
+                                            'paginator': paginator})
+
+
+def post_view(request, username, post_id):
+    member = User.objects.get(username=username)
+    post = member.posts.get(id=post_id)
+    post_list = Post.objects.filter(author=member)
+    post_count = len(post_list)
+    return render(request, "post.html", {'member': member,
+                                         'post': post,
+                                         'post_count': post_count})
+
+
+def post_edit(request, username, post_id):
+    return render(request, "post_new.html", {})
--- yatube/urls.py
+++ yatube/urls.py
@@ -9,14 +9,3 @@ urlpatterns = [
         path("auth/", include("django.contrib.auth.urls")),
         path("", include("posts.urls")),
 ]
-
-urlpatterns += [
-        path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
-        path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
-        path('about-author/', views.flatpage, {'url': '/about-author/'},
-             name='about-author'),
-        path('about-spec/', views.flatpage, {'url': '/about-spec/'},
-             name='about-spec'),
-        path('contacts/', views.flatpage, {'url': '/contacts/'},
-             name='contacts'),
-]
