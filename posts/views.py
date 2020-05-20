8b4b71e62d21120f11443aa0e9fa00235c5010dc
Илья <7634216@gmail.com>
2020-05-20 19:32:11 +0300

    edit posts

--- posts/views.py
+++ posts/views.py
@@ -37,32 +37,51 @@ def new_post(request):
             post.save()
             return redirect('index')
     form = PostForm()
-    return render(request, 'new_post.html', {'form': form})
+    return render(request, 'new_post.html', {'form': form,
+                                             'title': 'Добавить запись',
+                                             'button': 'Добавить'})
 
 
 def profile(request, username):
-    member = User.objects.get(username=username)
+    member = get_object_or_404(User, username=username)
     post_list = Post.objects.filter(author=member).order_by('-pub_date')
     post_count = len(post_list)
     paginator = Paginator(post_list, 10)
-
     page_number = request.GET.get('page')
-    page = paginator.get_page(page_number)
-    return render(request, "profile.html", {'member': member,
+    page = paginator.get_page(page_number)  # TODO: paginator's style
+    return render(request, 'profile.html', {'member': member,
                                             'post_count': post_count,
                                             'page': page,
                                             'paginator': paginator})
 
 
 def post_view(request, username, post_id):
-    member = User.objects.get(username=username)
-    post = member.posts.get(id=post_id)
+    member = get_object_or_404(User, username=username)
+    post = get_object_or_404(member.posts, id=post_id)
     post_list = Post.objects.filter(author=member)
     post_count = len(post_list)
-    return render(request, "post.html", {'member': member,
+    return render(request, 'post.html', {'member': member,
                                          'post': post,
                                          'post_count': post_count})
 
 
 def post_edit(request, username, post_id):
-    return render(request, "post_new.html", {})
+    edit = True
+    post = get_object_or_404(Post, id=post_id)
+    if post.author == request.user:
+        if request.method == 'POST':
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+                return redirect('post', username, post_id)
+        form = PostForm(instance=post)
+        return render(request, 'new_post.html',
+                      {'form': form,
+                       'edit': edit,
+                       'post': post,
+                       'username': username,
+                       'title': 'Редактировать запись',
+                       'button': 'Сохранить'}
+                      )
+    else:
+        return redirect('post', username, post_id)
