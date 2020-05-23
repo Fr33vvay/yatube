5394497785110aca9245708fa0c33f59b64462c8
Илья <7634216@gmail.com>
2020-05-23 19:39:39 +0300

    tests 1.5

--- posts/tests.py
+++ posts/tests.py
@@ -8,6 +8,11 @@ class TestHomeWork05(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='bum', password='12345')
         self.client.force_login(self.user)
+        self.post_text = 'New text'
+        self.edited_post_text = 'Edited text'
+        self.new_post = self.client.post(reverse('new_post'),
+                         {'text': self.post_text}, follow=True)
+
 
     def test_create_profile_after_reg(self):
         response = self.client.get(reverse('profile', kwargs={
@@ -17,8 +22,7 @@ class TestHomeWork05(TestCase):
                          msg='Профиль пользователя не создается')
 
     def test_new_post(self):
-        response = self.client.post(reverse('new_post'), {'text': 'New text'},
-                                    follow=True)
+        response = self.new_post
         self.assertEqual(response.status_code, 200, msg='Пост не создается')
         self.assertRedirects(response, reverse('index'),
                              msg_prefix='Нет редиректа на главную страницу')
@@ -30,51 +34,49 @@ class TestHomeWork05(TestCase):
                              status_code=302, target_status_code=200)
 
     def test_new_post_everywhere(self):
-        post = Post.objects.create(text='New text', author=self.user)
+        self.new_post
         resp_index = self.client.get(reverse('index'))
-        self.assertContains(resp_index, post.text,
+        self.assertContains(resp_index, self.post_text,
                             msg_prefix='Поста нет на главной странице')
 
         resp_profile = self.client.get(reverse('profile', kwargs={
             'username': self.user.username
         }))
-        self.assertContains(resp_profile, post.text,
+        self.assertContains(resp_profile, self.post_text,
                             msg_prefix='Поста нет на странице пользователя')
 
         resp_post = self.client.get(reverse('post', kwargs={
             'username': self.user.username,
             'post_id': 1
         }))
-        self.assertContains(resp_post, post.text,
+        self.assertContains(resp_post, self.post_text,
                             msg_prefix='Поста нет на его собственной странице')
 
     def test_edit_post(self):
-        self.client.post(reverse('new_post'), {'text': 'New text'},
-                         follow=True)
+        self.new_post
         response = self.client.post((reverse('post_edit', kwargs={
             'username': self.user.username,
             'post_id': 1
-        })), {'text': 'Edit text'}, follow=True)
+        })), {'text': self.edited_post_text}, follow=True)
         self.assertEqual(response.status_code, 200,
                          msg='Пост не редактируется')
 
     def test_edit_post_everywhere(self):
         self.client.force_login(self.user)
-        self.client.post(reverse('new_post'), {'text': 'New text'},
-                         follow=True)
+        self.new_post
         self.client.post((reverse('post_edit', kwargs={
             'username': self.user.username,
             'post_id': 1
-        })), {'text': 'Edit text'}, follow=True)
+        })), {'text': self.edited_post_text}, follow=True)
         resp_index = self.client.get(reverse('index'))
-        self.assertContains(resp_index, 'Edit text',
+        self.assertContains(resp_index, self.edited_post_text,
                             msg_prefix='Измененного поста нет на '
                                        'главной странице')
 
         resp_profile = self.client.get(reverse('profile', kwargs={
             'username': self.user.username
         }))
-        self.assertContains(resp_profile, 'Edit text',
+        self.assertContains(resp_profile, self.edited_post_text,
                             msg_prefix='Измененного поста нет на '
                                        'странице пользователя')
 
@@ -82,6 +84,6 @@ class TestHomeWork05(TestCase):
             'username': self.user.username,
             'post_id': 1
         }))
-        self.assertContains(resp_post, 'Edit text',
+        self.assertContains(resp_post, self.edited_post_text,
                             msg_prefix='Измененного поста нет на '
                                        'его собственной странице')
